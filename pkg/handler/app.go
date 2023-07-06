26916f4c25fe1361d3802850666749ea984a4b70
kgpp34 <974840768@qq.com>
2023-07-06 09:56:37 +0800

    fix: update event time to last timestamp

--- pkg/handler/app.go
+++ pkg/handler/app.go
@@ -61,10 +61,10 @@ func (h *Handler) getPodAndEvents(ns string, parentName string) []model.Instance
 
 		if len(events) > 0 {
 			for _, event := range events {
-				asiaTime, err := util.ConvertUTCToAsiaShanghai(event.CreationTimestamp.Time)
+				asiaTime, err := util.ConvertUTCToAsiaShanghai(event.LastTimestamp.Time)
 				if err != nil {
 					log.Errorf("解析时间出现错误:%v", err)
-					asiaTime = event.CreationTimestamp.Time.Format(time.RFC3339)
+					asiaTime = event.LastTimestamp.Time.Format(time.RFC3339)
 				}
 				instanceEvent := model.InstanceEvent{
 					Message: event.Message,
